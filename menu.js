8c5cb360f91f1f1e0f0e4eeeac2069c787e029fc
Shiro2116 <59782132+Shiro2116@users.noreply.github.com>
2020-09-01 00:48:45 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,24 +1,18 @@
 module.exports = `Hai, ini daftar menu yang bisa kalian gunakan disini
 
-*#Stiker/#Sticker/Stiker/stiker/#sticker/#stiker*
+*#sticker*
 _Bikin Stiker dari gambar 🖼, kirim gambar dan beri caption atau bisa juga quote/reply dari gambar yang sudah ada di chat/group_
-
 *#korona*
 _Data Korona Indonesia 🦠, data realtime dan terupdate dari sumber terpercaya_
-  
 *#quotes*
 _Random Quotes Bahasa Indonesia 🔖_
-
-*#wp/wallpaper*
+*#wp*
 _Random Wallpaper 🌈_
-
 *#zodiak(spasi)nama(spasi)tgl*
 _Cek Zodiak Kamu 🐟_
 _(contoh: #zodiak dandi 20-01-2005)_
-  
 *#kontak*
 _Kontak Owner_
-
 *#rules*
 _Rules Bot_
 
